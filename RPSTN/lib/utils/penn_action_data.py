1093617e0b0e4f4304e83d60289c835be82a63be
joseph <skdytpq98@naver.com>
2023-02-13 16:32:40 +0900

    f

--- RPSTN/lib/utils/penn_action_data.py
+++ RPSTN/lib/utils/penn_action_data.py
@@ -191,7 +191,7 @@ class Penn_Action(data.Dataset):
             l[i] = heatmap
             label_map[i] = transforms.ToTensor()(heatmap)
             # 가우시안 커널을 통해 label_map 즉, label의 관절 히트맵을 구한 것
-            pdb.set_trace()
+        pdb.set_trace()
         return images, label_map, label, img_paths, person_box, start_index,kpts # keypoints 좌표 
 # 이미지, 히트맵, 라벨(실제 참 값인 관절 좌표값) , 이미지 경로, 사람 바운딩 박스, 시작 지점
 
