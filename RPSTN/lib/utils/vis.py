da0961d5ba4c303515180d77da140cf24211b7b4
joseph <skdytpq98@naver.com>
2023-02-14 18:48:15 +0900

    f

--- RPSTN/lib/utils/vis.py
+++ RPSTN/lib/utils/vis.py
@@ -196,7 +196,7 @@ def save_batch_heatmaps(batch_image, batch_heatmaps, file_name,joints,
                         (int(preds[i][j][0]), int(preds[i][j][1])),
                         1, [0, 0, 255], 1)
             pdb.set_trace()
-            cv2.circle(resized_image,(int(joint[j,0],int(joint[j,1]))),1,(255, 0, 0),1)
+            cv2.circle(resized_image,(int(joint[j,0]),int(joint[j,1])),1,(255, 0, 0),1)
             width_begin = heatmap_width * (j+1)
             width_end = heatmap_width * (j+2)
             grid_image[height_begin:height_end, width_begin:width_end, :] = \
