0de94f054edfcd62be620573975aebf312cf777a
joseph <skdytpq98@naver.com>
2023-02-24 17:53:34 +0900

    f

--- train.py
+++ train.py
@@ -11,14 +11,21 @@ sys.path.append('RPSTN/pose_estimation')
 sys.path.append('RPSTN/lib/utils')
 # ITES, RPSTN 상대경로 지정 python RPSTN/pose_estimation/train_penn.py
 import os
-os.environ["KMP_DUPLICATE_LIB_OK"] = True
+import pdb
+from ITES.common.utils import deterministic_random
+#os.environ["KMP_DUPLICATE_LIB_OK"] = True
 from ITES import train_t
 from RPSTN.pose_estimation import train_penn
 from joint_heatmap import generate_2d_integral_preds_tensor
 from RPSTN.lib.utils import evaluate as evaluate
 from tensorboardX import SummaryWriter
+import argparse
+import torch
+from tqdm import tqdm
+import time
+import random
+import numpy as np
 def set_seed(seed):
-
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -35,7 +42,7 @@ class Trainer(object):
         self.dataset = args.dataset
         self.frame_memory = args.frame_memory   
         self.writer = args.writer
-        self.gpus = [int(i) for i in config.GPUS.split(',')]
+        self.gpus = 1#[int(i) for i in config.GPUS.split(',')]
         self.is_train = is_train
         self.is_visual = is_visual
         ## JRE
@@ -62,74 +69,20 @@ class Trainer(object):
         if self.dataset ==  "Penn_Action":
             self.numClasses = 13
             self.test_dir = None
-        def fetch(subjects, action_filter=None, subset=1, parse_3d_poses=True):
-            out_poses_3d = []
-            out_poses_2d = []
-            out_camera_params = []
-            for subject in subjects:
-                for action in keypoints[subject].keys():
-                    if action_filter is not None:
-                        found = False
-                        for a in action_filter:
-                            if action.startswith(a):
-                                found = True
-                                break
-                        if not found:
-                            continue
-
-                    poses_2d = keypoints[subject][action]
-                    for i in range(len(poses_2d)):  # Iterate across cameras
-                        out_poses_2d.append(poses_2d[i])
-
-                    if subject in dataset.cameras():
-                        cams = dataset.cameras()[subject]
-                        assert len(cams) == len(poses_2d), 'Camera count mismatch'
-                        for i,cam in enumerate(cams):
-                            if 'intrinsic' in cam:
-                                out_camera_params.append(np.tile((cam['intrinsic'])[None,:],(len(poses_2d[i]),1)))
-
-                    if parse_3d_poses and 'positions_3d' in dataset[subject][action]:
-                        poses_3d = dataset[subject][action]['positions_3d']
-                        assert len(poses_3d) == len(poses_2d), 'Camera count mismatch'
-                        for i in range(len(poses_3d)):  # Iterate across cameras
-                            out_poses_3d.append(poses_3d[i])
-
-            if len(out_camera_params) == 0:
-                out_camera_params = None
-            if len(out_poses_3d) == 0:
-                out_poses_3d = None
-
-            stride = args.downsample
-            if subset < 1:
-                for i in range(len(out_poses_2d)):
-                    n_frames = int(round(len(out_poses_2d[i]) // stride * subset) * stride)
-                    start = deterministic_random(0, len(out_poses_2d[i]) - n_frames + 1, str(len(out_poses_2d[i])))
-                    out_poses_2d[i] = out_poses_2d[i][start:start + n_frames:stride]
-                    if out_poses_3d is not None:
-                        out_poses_3d[i] = out_poses_3d[i][start:start + n_frames:stride]
-            elif stride > 1:
-                # Downsample as requested
-                for i in range(len(out_poses_2d)):
-                    out_poses_2d[i] = out_poses_2d[i][::stride]
-                    if out_poses_3d is not None:
-                        out_poses_3d[i] = out_poses_3d[i][::stride]
-                        out_camera_params[i] = out_camera_params[i][::stride]
-
-            return out_camera_params, out_poses_3d, out_poses_2d
-        self.train_loader, self.val_loader, self.test_loader = getDataloader(self.dataset, self.train_dir, \
+        self.train_loader, self.val_loader, self.test_loader = train_penn.getDataloader(self.dataset, self.train_dir, \
                                                                 self.val_dir, self.test_dir, self.sigma, self.stride, \
                                                                 self.workers, self.frame_memory, \
                                                                 self.batch_size)
         #loader output = images, label_map, label, img_paths, person_box, start_index,kpts
-        model_jre = train_penn.models.dkd_net.get_dkd_net(config, self.is_visual, is_train=True if self.is_train else False)
+        model_jre = train_penn.models.dkd_net.get_dkd_net(train_penn.config, self.is_visual, is_train=True if self.is_train else False)
 
-        model_pos_train = train_student.Teacher_net(self.num_joints,self.num_joints,2,  # joints = [13,2]
+        self.model_pos_train = train_t.Teacher_net(self.num_joints,self.num_joints,2,  # joints = [13,2]
                             n_fully_connected=self.n_fully_connected, n_layers=self.n_layers, 
                             dict_basis_size=self.basis, weight_init_std = self.init_std)
-        self.model_jre = torch.nn.DataParallel(model, device_ids=self.gpus).cuda()
-        self.criterion_jre = MSESequenceLoss().cuda()
+        self.model_jre = torch.nn.DataParallel(model_jre, device_ids=self.gpus).cuda()
+        self.criterion_jre = train_penn.MSESequenceLoss().cuda()
         self.optimizer_jre = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        self.optimizer_ite = torch.optim.SGD(model_pos_train.parameters(), lr=lr,
+        self.optimizer_ite = torch.optim.SGD(self.model_pos_train.parameters(), lr=self.lr,
                             momentum=args.momentum,
                             weight_decay=args.weight_decay)
 
@@ -165,7 +118,7 @@ class Trainer(object):
         print("Epoch " + str(epoch) + ':') 
         tbar = tqdm(self.train_loader)
         for i, (input, heatmap, label, img_path, bbox, start_index, kpts) in enumerate(tbar):
-            learning_rate = adjust_learning_rate(self.optimizer, epoch, self.lr, weight_decay=self.weight_decay, policy='multi_step',
+            learning_rate = train_penn.adjust_learning_rate(self.optimizer, epoch, self.lr, weight_decay=self.weight_decay, policy='multi_step',
                                                  gamma=self.gamma, step_size=self.step_size)
 
             vis = label[:, :, :, -1]
@@ -181,7 +134,7 @@ class Trainer(object):
                     file_name = 'result/heats/train/{}_batch.jpg'.format(epoch)
                     input = input.view(-1, c, h, w)
                     heat = heat.view(-1, 16, heat.shape[-2], heat.shape[-1])
-                    save_batch_heatmaps(input,heat,file_name)
+                    train_penn.save_batch_heatmaps(input,heat,file_name)
 
             kpts = kpts[:16] # joint
             heat = torch.zeros(self.numClasses, self.heatmap_size, self.heatmap_size).cuda()
@@ -192,7 +145,7 @@ class Trainer(object):
             heat = self.model(input_var)
             losses = self.criterion(heat, heatmap_var)
             jfh  = generate_2d_integral_preds_tensor(heat , self.num_joints, self.heatmap_size,self.heatmap_size) # joint from heatmap K , 64 , 64 
-            preds = model_pos_train(jfh,align_to_root=True)
+            preds = self.model_pos_train(jfh,align_to_root=True)
             # Batch, 16,2
             loss_total =  loss_reprojection + loss_consistancy
             loss_reprojection = preds['l_reprojection'] 
@@ -252,4 +205,17 @@ class Trainer(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pretrained', default=None, type=str, dest='pretrained')
+    parser.add_argument('--dataset', type=str, dest='dataset', default='Penn_Action')
+    parser.add_argument('--train_dir', default=None,type=str, dest='train_dir')
+    parser.add_argument('--val_dir', type=str, dest='val_dir', default=None)
+    parser.add_argument('--model_name', default='', type=str)
+    parser.add_argument('--model_save_path', default='experiments/checkpoint/')
+    parser.add_argument('--source_model_save_path', default='experiments/source-model/')
+    parser.add_argument('--model_arch', default=None, type=str)
+    parser.add_argument('--writer', default=None)
+    parser.add_argument('--is_train', default=False, type=bool)
+    parser.add_argument('--visual', default=False, type=bool, help='If visualize results')
+    parser.add_argument('--dir' , default = 'run',type=str)
     parser.add_argument('--pretrained_jre', default=None, type=str, dest='pretrained')
