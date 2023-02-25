1156fd3a909d93251269623efb10180ac747a839
joseph <skdytpq98@naver.com>
2023-02-25 17:09:23 +0900

    f

--- train.py
+++ train.py
@@ -167,9 +167,10 @@ class Trainer(object):
             self.writer.add_scalar('loss_jre', (losses / self.batch_size), epoch)
             self.writer.add_scalar('total_loss', (loss_total / self.batch_size), epoch)
             self.writer.add_scalar('teacher_loss', (train_loss / self.batch_size), epoch)
-            vis_joint = preds['shape_camera_coord']
-            if epoch % 5 == 0 :
-                draw_3d_pose(vis_joint[0,:,:],f'exp/vis/{epoch}.jpg')
+            with torch.no_grad():
+                vis_joint = preds['shape_camera_coord']
+                if epoch % 5 == 0 :
+                    draw_3d_pose(vis_joint[0,:,:],f'exp/vis/{epoch}.jpg')
 
             # output => [ba , num_joints , 2]
     def validation(self, epoch):
