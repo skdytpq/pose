34b0464f15ad2d4d59e0fc033a7027e0b1a0850c
joseph <skdytpq98@naver.com>
2023-03-10 16:42:50 +0900

    f

--- data/human.py
+++ data/human.py
@@ -1,6 +1,6 @@
 import tarfile
 import os
-base = os.listdir('data/cdf')
+base = os.listdir('data/file')
 for i in base:
     with tarfile.open(os.path.join(base,i), 'r:gz') as tr:
-        tr.extractall()
+        tr.extractall('data/cdf')
