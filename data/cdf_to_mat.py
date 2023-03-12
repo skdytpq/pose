4edec8cb755440639ba0af1a8599265b35c6cb34
joseph <skdytpq98@naver.com>
2023-03-12 19:25:17 +0900

    f

--- data/cdf_to_mat.py
+++ data/cdf_to_mat.py
@@ -9,12 +9,12 @@ subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
 for subject in subjects:
     sub_path = os.path.join(f'cdf/{subject}', 'MyPoseFeatures/D3_Positions')
     sub_path2 = os.path.join(f'cdf/{subject}_mat', 'MyPoseFeatures/D3_Positions')
+    os.makedirs(sub_path2,exist_ok=True)
     dirs = os.listdir(os.path.join(f'cdf/{subject}', 'MyPoseFeatures/D3_Positions'))
     for filename in dirs:
         if filename.endswith('.cdf'):
             path = os.path.join(sub_path, filename)
             data = cdflib.CDF(path)
-            pdb.set_trace()
             mat_filename = os.path.splitext(filename)[0] + '.mat'
             mat_path = os.path.join(sub_path2, mat_filename)
             sio.savemat(mat_path, {'data': data[0]})
