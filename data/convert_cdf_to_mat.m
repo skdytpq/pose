bf2b265ab17e09be19eb6bfb544d101e918d8cfb
joseph <skdytpq98@naver.com>
2023-03-12 16:27:35 +0900

    f

---
+++ data/convert_cdf_to_mat.m
@@ -0,0 +1,12 @@
+
+
+pose_directory = 'cdf';
+dirs = dir(strcat(pose_directory, '/*/MyPoseFeatures/D3_Positions/*.cdf'));
+
+paths = {dirs.folder};
+names = {dirs.name};
+
+for i = 1:numel(names)
+    data = cdfread(strcat(paths{i}, '/', names{i}));
+    save(strcat(paths{i}, '/', names{i}, '.mat'), 'data');
+end
