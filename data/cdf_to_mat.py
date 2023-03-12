b9d099f88ccc85f8ad43a8e77ac36b87a9ebb407
joseph <skdytpq98@naver.com>
2023-03-12 17:16:20 +0900

    f

--- data/cdf_to_mat.py
+++ data/cdf_to_mat.py
@@ -1,6 +1,6 @@
 import os
 import scipy.io as sio
-from oct2py import octave
+import oct2py
 
 pose_directory = 'cdf'
 subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
@@ -9,7 +9,7 @@ for subject in subjects:
     for filename in dirs:
         if filename.endswith('.cdf'):
             path = os.path.join(dirs, filename)
-            data = octave.cdfread(path)
+            data = oct2py.cdfread(path)
             mat_filename = os.path.splitext(filename)[0] + '.mat'
             mat_path = os.path.join(dirs, mat_filename)
             sio.savemat(mat_path, {'data': data})
