c1cbd1f26f2752989a58a2132fa672d3033ff39b
joseph <skdytpq98@naver.com>
2023-03-12 17:27:06 +0900

    f

--- data/cdf_to_mat.py
+++ data/cdf_to_mat.py
@@ -1,8 +1,6 @@
 import os
 import scipy.io as sio
 import os
-os.environ["CDF_LIB"] = "~/CDF/lib"
-from spacepy import pycdf
 
 pose_directory = 'cdf'
 subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
@@ -12,6 +10,8 @@ for subject in subjects:
     for filename in dirs:
         if filename.endswith('.cdf'):
             path = os.path.join(sub_path, filename)
+            os.environ["CDF_LIB"] = f"/home/work/pose/pose/data/cdf/{subject}/MyPoseFeatures/D3_Positions"
+            from spacepy import pycdf
             data = pycdf.CDF(path)
             mat_filename = os.path.splitext(filename)[0] + '.mat'
             mat_path = os.path.join(dirs, mat_filename)
