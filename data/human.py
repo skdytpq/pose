6d482d517e97a007b2f7df75dfea7f28ea6f3afa
joseph <skdytpq98@naver.com>
2023-03-13 09:41:38 +0900

    f

--- data/human.py
+++ data/human.py
@@ -2,6 +2,6 @@ import tarfile
 import os
 base = os.listdir('data/monofile')
 for i in base:
-    with tarfile.open(os.path.join('data/file',i), 'r:gz') as tr:
+    with tarfile.open(os.path.join('data/monofile',i), 'r:gz') as tr:
         tr.extractall('data/monocdf')   
         
