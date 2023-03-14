908eb0817d9480d1b71e68adc867e7db574b9d34
joseph <skdytpq98@naver.com>
2023-03-14 17:33:52 +0900

    f

--- RPSTN/pose_estimation/train_penn.py
+++ RPSTN/pose_estimation/train_penn.py
@@ -134,6 +134,7 @@ class Trainer(object):
             loss = 0
             start_model = time.time()
             heat = self.model(input_var)
+            pdb.set_trace()
             losses = self.criterion(heat, heatmap_var)
 
 
