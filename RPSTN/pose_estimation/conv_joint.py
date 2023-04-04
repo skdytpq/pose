8fb2b38fae2f26bd0935cec2062925d74db6809f
joseph <skdytpq98@naver.com>
2023-04-04 15:55:16 +0900

    f

--- RPSTN/pose_estimation/conv_joint.py
+++ RPSTN/pose_estimation/conv_joint.py
@@ -13,11 +13,11 @@ class heatconv(nn.Module):
         self.heatmap_size = heatmap_size
         self.n_fully_connected = n_fully_connected
         self.n_layers = n_layers
-        self.num_joints = num_joints
+        self.num_joints =13 # num_joints
         self.fe_net = nn.Sequential(
-                *self.make_trunk(dim_in=self.num_joints,
-                                n_fully_connected=self.n_fully_connected,
-                                n_layers=self.n_layers)) # Convolution Batchnormailization fully connected layer
+            *self.make_trunk(dim_in=self.num_joints ,
+                             n_fully_connected=self.n_fully_connected,
+                             n_layers=self.n_layers)) # Convolution Batchnormailization fully connected layer
     def make_trunk(self,
                    n_fully_connected=None,
                    dim_in=None,
