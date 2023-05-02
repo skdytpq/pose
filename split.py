f0937765173337c60af4b031910b4580e2d4916c
joseph <skdytpq98@naver.com>
2023-05-02 15:30:34 +0900

    f

--- split.py
+++ split.py
@@ -15,7 +15,7 @@ def move_visible(files,i,path):
             shutil.move(f'data/pose_data/{i}/{fil}',f'data/pose_data/itedata/{i}/{fil}')
 
 for i in ['train','test']:
-    lis = os.path.join(f'data/pose/data/{i}')
+    lis = os.path.join(f'data/pose_data/{i}')
     for file in os.listdir(lis):
         path = os.path.join(lis,file)
         move_visible(file,i,path)
