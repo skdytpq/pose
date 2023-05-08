7a2edc03d6a21c585131207dfc470c9b13040774
joseph <skdytpq98@naver.com>
2023-05-08 10:33:26 +0900

    f

--- train.py
+++ train.py
@@ -144,7 +144,7 @@ class Trainer(object):
         self.criterion_jre = train_penn.MSESequenceLoss().cuda()
         if args.sub_trained:
             pdb.set_trace()
-            self.submodel.load_state_dict(torch.load('exp/submodel/best_submodule.bin.bin')['model_pos'],strict = False)
+            self.submodel.load_state_dict(torch.load('exp/submodel/best_submodule.bin')['model_pos'],strict = False)
         if args.pretrained:
             self.param = list(self.model_pos_train.parameters())
         else:
