b6e46010a6b21a62bdb263f4f80710e5106adc60
joseph <skdytpq98@naver.com>
2023-06-25 20:53:42 +0900

    f

--- train.py
+++ train.py
@@ -426,12 +426,12 @@ if __name__ == '__main__':
     args.dataset  = 'pose_data'
     args.frame_memory = 5
     if args.dataset == 'pose_data':
-        args.train_dir  = '../data/pose_data/itedata'
-        args.val_dir    = '../data/pose_data/itedata'
+        args.train_dir  = 'data/pose_data/itedata'
+        args.val_dir    = 'data/pose_data/itedata'
         tb_log_dir = 'run/'
     else:
-        args.train_dir  = '../data/pose_data'
-        args.val_dir    = '../data/pose_data'
+        args.train_dir  = 'data/pose_data'
+        args.val_dir    = 'data/pose_data'
   #  tb_log_dir = 'run/penn/'
 #    writer = SummaryWriter(log_dir= '', comment='weight_decay')
 #    args.writer = writer
