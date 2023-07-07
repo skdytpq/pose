f29765722665b77cff1f09eeaf38c8a215123ab2
joseph <skdytpq98@naver.com>
2023-07-08 02:34:33 +0900

    f

--- train3.py
+++ train3.py
@@ -361,7 +361,7 @@ while epoch < args.epochs:
             errors_3d_valid_p1[-1] * 1000,
             errors_3d_valid_p2[-1] * 1000))
         ft = open('MPJPE/loger_sub_masking1.txt','w')
-        ft.write('errors_3d_valid_p2[-1] * 1000\n')
+        ft.write(f'{errors_3d_valid_p2[-1] * 1000}\n')
         ft.close()
     # Decay learning rate exponentially
     if (epoch+1) % args.epoch_lr_decay == 0:
