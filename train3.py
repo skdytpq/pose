58ff93adc00a85995fd257905ba6a2d28695d282
joseph <skdytpq98@naver.com>
2023-07-07 22:57:58 +0900

    f

--- train3.py
+++ train3.py
@@ -251,6 +251,7 @@ while epoch < args.epochs:
             inputs_2d = inputs_2d.cuda()
         sub_input = submodel(inputs_2d)
         sub_input = sub_input.values()
+        pdb.set_trace()
         optimizer.zero_grad()
         if epoch < 15:
             preds = model_pos_train(sub_input,align_to_root=True)
