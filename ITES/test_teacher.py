c0142991d4cd4d1b39ce2a3b1cd8c65798dbd1dd
joseph <skdytpq98@naver.com>
2023-07-24 16:28:31 +0900

    f

--- ITES/test_teacher.py
+++ ITES/test_teacher.py
@@ -230,6 +230,7 @@ if args.evaluate:
             if torch.cuda.is_available():
                 inputs_3d = inputs_3d.cuda()
                 inputs_2d = inputs_2d.cuda()
+            pdb.set_trace()
             inputs_2d = mask_joint(inputs_2d)
             inputs_2d = submodel(inputs_2d)
             preds = model_pos(inputs_2d)
