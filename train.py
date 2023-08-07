c4f20b18f2947cec2a76d7b52c44799bd4053797
joseph <skdytpq98@naver.com>
2023-08-08 02:13:03 +0900

    f

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ class Trainer(object):
         ## JRE
         self.writer = SummaryWriter('exp/tensor/3d')
         self.test_dir = None
-        self.workers = 12
+        self.workers = 4
         self.weight_decay = 0.1
         self.momentum = 0.9
         self.batch_size = args.batch
