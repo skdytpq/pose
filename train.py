e7db773a8fdfe234d25cfbc1d6dc1e872ecbe9c6
joseph <skdytpq98@naver.com>
2023-08-08 10:26:58 +0900

    f

--- train.py
+++ train.py
@@ -34,6 +34,7 @@ from ITES.common.function import *
 from reconstruct_joint import Student_net
 
 
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 dataset_path = 'ITES/data/data_3d_' + 'h36m'+ '.npz'
 dataset = Human36mDataset(dataset_path)
@@ -85,7 +86,7 @@ def make_joint(jfh):
 
 class Trainer(object):
     def __init__(self, args, is_train, is_visual):
-        os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
+        os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2'
         self.args = args
         self.train_dir = args.train_dir
         self.val_dir = args.val_dir
@@ -138,7 +139,7 @@ class Trainer(object):
         loaded_state_dict = torch.load('exp/checkpoints/penn_train_20230624_best.pth.tar')['state_dict']
         self.submodel = Student_net(adj, self.hid_dim, num_layers=self.n_blocks, p_dropout=0.0,
                        nodes_group=dataset.skeleton().joints_group())
-        self.submodel = torch.nn.DataParallel(self.submodel, device_ids=self.gpus).to('cuda')
+        self.submodel = torch.nn.DataParallel(self.submodel, device_ids=[0,1,2],output_device= 1).to('cuda')
         self.model_jre.load_state_dict(loaded_state_dict)
         if args.pretrained:
             #self.model_jre.load_state_dict(torch.load(args.pretrained)['state_dict'])
@@ -223,6 +224,8 @@ class Trainer(object):
                 reconstruct = preds['reconstruct']
                 reconstruct = reconstruct.to('cuda')
                 train_loss = self.criterion_jre(kpts,reconstruct)
+                print("Outside: input size", kpts_mask.size(),
+          "output_size", reconstruct.size())
 
             else:
                 #jfh_mask = mask_joint(jfh)
