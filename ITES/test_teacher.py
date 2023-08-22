666712577f2f3373b1dce34276de11b8fbc97f2b
joseph <skdytpq98@naver.com>
2023-08-22 23:04:38 +0900

    f

--- ITES/test_teacher.py
+++ ITES/test_teacher.py
@@ -241,7 +241,7 @@ if args.evaluate:
                 inputs_3d = inputs_3d.cuda()
                 inputs_2d = inputs_2d.cuda()
             #inputs_2d_ = mask_joint(inputs_2d)
-            inputs_2d = submodel_pos(inputs_2d)['keypoints_2d']
+            #inputs_2d = submodel_pos(inputs_2d)['keypoints_2d']
             preds = model_pos(inputs_2d)
 
             shape_camera_coord = preds['shape_camera_coord']
